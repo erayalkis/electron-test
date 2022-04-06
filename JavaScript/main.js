468c298dc8ec17db09d09d9f00f4dc1286124cdb
Lily <lolpo12345@outlook.com>
2022-04-06 22:44:17 +0300

    Add code to main.js

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,1 +1,14 @@
 const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const wnd = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  wnd.loadFile("../HTML/index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
